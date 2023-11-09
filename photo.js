311aaeb95690cf94fb94215c2cc7988703eae1db
kazuma1112 <kazuma1112.mainmail@gmail.com>
2023-11-09 17:42:10 +0900

    写真専用ページの追加

---
+++ photo.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const photoContainer = document.getElementById("photoContainer");
+
+    fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            // data.json ファイルから写真の情報を取得
+            const photos = Object.keys(data).map(key => ({ id: key, ...data[key], title: data[key].title.trim() || 'デフォルトタイトル' }));
+
+            // 写真を表示するためのHTMLを生成
+            const photoHTML = photos.map(photo => `<div class="photo">
+            <p>${photo.title}</p>
+                <img src="data/${photo.filename}" alt="${photo.title}">
+            </div>`).join('');
+
+            // 写真とタイトルをphotoContainerに追加
+            photoContainer.innerHTML = photoHTML;
+        })
+        .catch(error => console.error("写真の読み込みエラー", error));
+});
